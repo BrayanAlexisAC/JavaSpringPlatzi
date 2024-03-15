ff61e26255c7530b4058cb30aa33e2a215692bf4
Brayan Alexis Aguayo Contreras <brayan.aguayo@keyrus.com>
2024-03-15 17:05:00 -0600

    move file trainingController to web/controllers

--- src/main/java/com/training/spring/web/controllers/trainingController.java
+++ src/main/java/com/training/spring/web/controllers/trainingController.java
@@ -1,4 +1,4 @@
-package com.training.spring.controllers;
+package com.training.spring.web.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
