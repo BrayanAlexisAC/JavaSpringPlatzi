4a9d6e468ff1768bd5379a8a87396801026685e4
Brayan Alexis Aguayo Contreras <brayan.aguayo@keyrus.com>
2024-03-19 16:07:34 -0600

    add getters and setters to object CategoryModel

--- src/main/java/com/training/spring/persistence/entity/CategoryModel.java
+++ src/main/java/com/training/spring/persistence/entity/CategoryModel.java
@@ -16,4 +16,28 @@ public class CategoryModel {
 
     @Column(name = "estado")
     private Boolean available;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getAvailable() {
+        return available;
+    }
+
+    public void setAvailable(Boolean available) {
+        this.available = available;
+    }
 }
