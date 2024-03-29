c9a5b59c0ac03864faaf942fecfb99be2dc564db
Brayan Alexis Aguayo Contreras <brayan.aguayo@keyrus.com>
2024-03-29 13:51:19 -0600

    Add Tag in the OrderController

--- src/main/java/com/training/spring/web/controllers/OrderController.java
+++ src/main/java/com/training/spring/web/controllers/OrderController.java
@@ -8,6 +8,7 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +20,7 @@ import java.util.Objects;
 
 @RestController
 @RequestMapping("/order")
+@Tag(name = "Orders Controller", description = "Get just information about orders")
 public class OrderController {
 
     @Autowired
