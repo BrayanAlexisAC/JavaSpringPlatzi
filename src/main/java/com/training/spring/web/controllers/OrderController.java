5ae4d109c00d80030de892aa61f8942f79a56410
Brayan Alexis Aguayo Contreras <brayan.aguayo@keyrus.com>
2024-03-29 13:09:18 -0600

    Implemented swagger 3 and documented API's product and orders

--- src/main/java/com/training/spring/web/controllers/OrderController.java
+++ src/main/java/com/training/spring/web/controllers/OrderController.java
@@ -2,6 +2,12 @@ package com.training.spring.web.controllers;
 
 import com.training.spring.domain.data.PucharseData;
 import com.training.spring.domain.services.PucharseService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,20 +25,50 @@ public class OrderController {
     private PucharseService pucharseService;
 
     @GetMapping("/all")
+    @Operation(
+            summary = "Get all purchases",
+            method = "GET",
+            operationId = "getAllPurchases"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "List of all purchases"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content(schema = @Schema())),
+            @ApiResponse(responseCode = "404", description = "Empty List", content = @Content(schema = @Schema()))
+    })
     public ResponseEntity<List<PucharseData>> getAll(){
         List<PucharseData> lstPucharses = pucharseService.getAll();
-        return lstPucharses.isEmpty() ? new ResponseEntity<>(HttpStatus.NOT_FOUND) : new ResponseEntity<>(lstPucharses, HttpStatus.OK);
+        return lstPucharses.isEmpty() ? new ResponseEntity<>(Collections.emptyList(),HttpStatus.NOT_FOUND) : new ResponseEntity<>(lstPucharses, HttpStatus.OK);
     }
 
     @GetMapping("/customer/{idCustomer}")
-    public ResponseEntity<List<PucharseData>> getPucharsesByCient(@PathVariable String idCustomer){
+    @Operation(
+            summary = "Get purchases by client ID",
+            method = "GET",
+            operationId = "getPurchasesByClientId"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "List of purchases for the specified client"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content(schema = @Schema())),
+            @ApiResponse(responseCode = "404", description = "Error code", content = @Content(schema = @Schema()))
+    })
+    public ResponseEntity<List<PucharseData>> getPucharsesByCient(@Parameter(description = "ID of the client", example = "7", required = true) @PathVariable String idCustomer){
         return pucharseService.getPucharsesByClient(idCustomer)
                 .map(pucharsesData -> new ResponseEntity<>(pucharsesData, HttpStatus.OK))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @PostMapping("/save")
-    public ResponseEntity<PucharseData> save(@RequestBody PucharseData pucharseData){
+    @Operation(
+            summary = "Save a purchase",
+            method = "POST",
+            operationId = "savePurchase"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Purchase saved successfully"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content(schema = @Schema())),
+            @ApiResponse(responseCode = "406", description = "Purchase could not be saved due to invalid data", content = @Content(schema = @Schema()))
+    })
+    public ResponseEntity<PucharseData> save(@Parameter(description = "Purchase data to be saved", required = true) @RequestBody PucharseData pucharseData){
         PucharseData savePucharseData = pucharseService.save(pucharseData);
         return Objects.nonNull(savePucharseData) && savePucharseData.getPucharseId() > 0 ?
                 new ResponseEntity<>(savePucharseData, HttpStatus.CREATED) : new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
--- src/main/java/com/training/spring/web/controllers/ProductController.java
+++ src/main/java/com/training/spring/web/controllers/ProductController.java
@@ -2,6 +2,13 @@ package com.training.spring.web.controllers;
 
 import com.training.spring.domain.data.ProductData;
 import com.training.spring.domain.services.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +22,7 @@ import java.util.Objects;
 
 @RestController
 @RequestMapping("/products")
+@Tag(name = "Products Controller", description = "Get back only information about products")
 public class ProductController {
     Logger log = LoggerFactory.getLogger(ProductController.class);
 
@@ -22,23 +30,53 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping("/all")
+    @Operation(
+            method = "GET",
+            operationId = "all",
+            summary = "Get back all Products in the database",
+            description = "Get back all products without exceptions")
+    @ApiResponses({
+            @ApiResponse(responseCode =  "200", description = "Get back a product list"),
+            @ApiResponse(responseCode =  "401", description = "Wrong credentials, Forbidden", content = @Content(schema = @Schema())),
+            @ApiResponse(responseCode =  "404", description = "Get back an empty list", content = @Content(schema = @Schema()))
+    })
     public ResponseEntity<List<ProductData> > getAll(){
         List<ProductData> lstProducts = productService.getAll();
         if (lstProducts.isEmpty()){
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>(Collections.emptyList(), HttpStatus.NOT_FOUND);
         } else {
             return new ResponseEntity<>(lstProducts, HttpStatus.OK);
         }
     }
 
     @GetMapping("/{productId}")
-    public ResponseEntity<ProductData> getProduct(@PathVariable("productId") int productId){
+    @Operation(
+            method = "GET",
+            operationId = "productId",
+            summary = "Get a product",
+            description = "Get all information about a product")
+    @ApiResponses({
+            @ApiResponse(responseCode =  "200", description = "Get a product information"),
+            @ApiResponse(responseCode =  "401", description = "Wrong credentials, Forbidden", content = @Content(schema = @Schema())),
+            @ApiResponse(responseCode =  "404", description = "Get back an error code", content = @Content(schema = @Schema()))
+    })
+    public ResponseEntity<ProductData> getProduct(@Parameter(description = "Product identifier", example = "10", required = true) @PathVariable("productId") int productId){
         ProductData productData = productService.getProductById(productId);
         return Objects.nonNull(productData) ? new ResponseEntity<>(productData, HttpStatus.ACCEPTED) : new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @GetMapping("/category/{categoryId}")
-    public ResponseEntity<List<ProductData>> getProductsByCategory(@PathVariable("categoryId") int categoryId){
+    @Operation(
+            method = "GET",
+            operationId = "category/categoryId",
+            summary = "Get products in a category",
+            description = "Get everything products in an specific category")
+    @ApiResponses({
+            @ApiResponse(responseCode =  "200", description = "Get a product list"),
+            @ApiResponse(responseCode =  "401", description = "Wrong credentials, Forbidden", content = @Content(schema = @Schema())),
+            @ApiResponse(responseCode =  "404", description = "Returns an error code", content = @Content(schema = @Schema()))
+    })
+    public ResponseEntity<List<ProductData>> getProductsByCategory(@Parameter(description = "Category identifier", example = "5", required = true) @PathVariable("categoryId") int categoryId){
         List<ProductData> lstProducts = productService.getProductsByCategory(categoryId);
         if (lstProducts.isEmpty()){
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
@@ -48,6 +86,16 @@ public class ProductController {
     }
 
     @GetMapping("/stock/{maxStock}")
+    @Operation(
+            method = "GET",
+            operationId = "stock/maxStock",
+            summary = "Get products less specified stock",
+            description = "Get products with stock less than or equal to the specified maximum stock")
+    @ApiResponses({
+            @ApiResponse(responseCode =  "200", description = "Get a product list"),
+            @ApiResponse(responseCode =  "401", description = "Wrong credentials, Forbidden", content = @Content(schema = @Schema())),
+            @ApiResponse(responseCode =  "404", description = "Returns an error code", content = @Content(schema = @Schema()))
+    })
     public ResponseEntity<List<ProductData>> getProductsLessStock(@PathVariable("maxStock") int maxStock){
         List<ProductData> lstProducts = productService.getProductsByStockAndAvailable(maxStock);
         if (lstProducts.isEmpty()){
@@ -58,13 +106,32 @@ public class ProductController {
     }
 
     @PostMapping("/save")
-    public ResponseEntity<ProductData> save(@RequestBody ProductData productData){
+    @Operation(
+            method = "POST",
+            operationId = "save",
+            summary = "Save a product")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Product saved successfully"),
+            @ApiResponse(responseCode = "401", description = "Wrong credentials, Forbidden", content = @Content(schema = @Schema())),
+            @ApiResponse(responseCode = "406", description = "Product could not be saved due to invalid data", content = @Content(schema = @Schema())),
+    })
+    public ResponseEntity<ProductData> save(@Parameter(description = "Product data to be saved", required = true) @RequestBody ProductData productData){
         ProductData saveProduct = productService.save(productData);
         return Objects.nonNull(saveProduct) && saveProduct.getId() > 0 ? new ResponseEntity<>(saveProduct, HttpStatus.CREATED) : new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
     }
 
     @DeleteMapping("/delete/{productId}")
-    public ResponseEntity<Object> delete(@PathVariable("productId") int productId) throws Exception {
+    @Operation(
+            summary = "Delete a product by ID",
+            method = "DELETE",
+            operationId = "deleteProductById"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Product deleted successfully"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content(schema = @Schema())),
+            @ApiResponse(responseCode = "406", description = "Product could not be deleted", content = @Content(schema = @Schema()))
+    })
+    public ResponseEntity<Object> delete(@Parameter(description = "ID of the product to delete", example = "10", required = true) @PathVariable("productId") int productId) throws Exception {
         ProductData productData = productService.getProductById(productId);
         return productService.delete(productData) ? new ResponseEntity<>(HttpStatus.OK) : new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
     }
